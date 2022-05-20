0695f6dcfad1a5d7672789ced53480b28c3bd296
Paul-Berdier <paul.berdier@ynov.com>
2022-05-20 09:30:01 +0200

    init:player(def) + init:board

--- src/player/Player.py
+++ src/player/Player.py
@@ -1,7 +1,26 @@
+import random
+
+
 class Player:
 
     base_account = 500
+    min_dice = 1
+    max_dice = 6
 
-    def __init__(self):
+    def __init__(self, name):
         self.account = Player.base_account
-        sel
+        self.name = name
+        self.last_number_dice = 0
+        self.ground_account = []
+
+    def roll(self):
+        self.last_number_dice = random.randint(Player.min_dice, Player.max_dice)
+
+    def get_last_number_dice(self):
+        return self.last_number_dice
+
+    def add_ground(self):
+        pass
+
+    def del_ground(self):
+        pass
